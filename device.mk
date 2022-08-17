d340fe56cbf4fda11bcf3b516a80cd52999345b2
Taqi Raza <mttalpur@gmail.com>
2022-08-18 00:47:01 +0500

    Update device mk
    add oem ota cert

--- device.mk
+++ device.mk
@@ -71,3 +71,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/recovery/root/system/etc/security/xiaomi
