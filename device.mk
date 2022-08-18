5ea28154984645b806abf255d82a08190315aa08
Taqi Raza <mttalpur@gmail.com>
2022-08-18 11:24:02 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -65,6 +65,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-impl \
     android.hardware.boot@1.1-mtkimpl.recovery \
     android.hardware.boot@1.1-mtkimpl
 
