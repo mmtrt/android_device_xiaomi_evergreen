3c505f7e090bd8590284936c5e04db3d2cf89478
Taqi Raza <mttalpur@gmail.com>
2022-08-19 20:27:08 +0500

    Update dt

--- device.mk
+++ device.mk
@@ -64,8 +64,10 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-impl
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl
 
 # Fastbootd
 PRODUCT_PACKAGES += \
