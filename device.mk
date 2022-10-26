ef3b26b9febfcc928b5f7cc7ac3b6f56fbc445fa
Taqi Raza <mttalpur@gmail.com>
2022-10-26 09:44:33 +0500

    Update dt

--- device.mk
+++ device.mk
@@ -88,7 +88,11 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 # Additional Target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1
+    android.hardware.keymaster@4.1 \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
