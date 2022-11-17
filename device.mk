fd5a5df44658585a671c14f238644f9424f214d3
Taqi Raza <mttalpur@gmail.com>
2022-11-17 09:36:58 +0500

    Update repo

--- device.mk
+++ device.mk
@@ -90,15 +90,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Gatekeeper
-PRODUCT_PACKAGES += \
-	android.hardware.gatekeeper@1.0-service \
-	android.hardware.gatekeeper@1.0-impl
-
-PRODUCT_COPY_FILES += \
-	$(OUT_DIR)/target/product/evergreen/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.gatekeeper@1.0-service \
-	$(OUT_DIR)/target/product/evergreen/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
